8442cb7e41714994dfd0de24c3cf5b93fe56c97b
SweatBoyz <cclownyasha@example.com>
2025-03-05 19:06:19 +0200

    Initial commit

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const app = express();
 const server = app.listen(3000, () => console.log('Server started on http://localhost:3000'));
 const wss = new WebSocket.Server({ server });
 
+// Указываем, что папка "public" содержит статику
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Если по пути / ничего не найдено, то отправляем index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Обработчик WebSocket-соединений
 wss.on('connection', ws => {
     console.log('Player connected');
+    
     ws.on('message', message => {
         console.log('Received: %s', message);
         wss.clients.forEach(client => {
@@ -17,5 +25,9 @@ wss.on('connection', ws => {
                 client.send(message);
             }
         });
+    });
+
+    ws.on('close', () => {
+        console.log('Player disconnected');
     });
 });
